6d5fc33c3c531207d08074b3aec3efba79a683cb
Pacôme Françon <pacome.francon@epitech.eu>
2018-11-21 16:34:33 +0100

    merge zetapush hello world with React

--- hello-world-react/src/App.js
+++ hello-world-react/src/App.js
@@ -1,13 +1,42 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import reactLogo from './logo.svg';
+import zetapushLogo from './logo.png';
 import './App.css';
 
+import { WeakClient } from '@zetapush/client';
+
 class App extends Component {
+
+  constructor(client, api) {
+    super(client, api)
+
+    client = new WeakClient({
+      platformUrl: 'https://celtia.zetapush.com/zbo/pub/business',
+      appName: '2s6dq7oo1'
+    });
+    api = client.createProxyTaskService();
+    client.connect().then(() =>
+      document.body.classList.add('connected')
+    );
+  }
+
+  async onClick() {
+    console.log(await this.api.hello());
+  }
+
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
+          <img src={reactLogo} className="App-logo" alt="reactLogo" />
+          <img src={zetapushLogo} className="App-logo" alt="zetapushLogo" />
+          <h1>Welcome to ZetaPush with React</h1>
+
+          <h2>To get started, edit worker/index.ts and save to reload.</h2>
+          <nav>
+            <button class="js-Hello" onClick={this.onClick}>call hello()</button>
+          </nav>
+
           <p>
             Edit <code>src/App.js</code> and save to reload.
           </p>
