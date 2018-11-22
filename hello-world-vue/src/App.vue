2877d2fe4c1fd7366617c8b323abecac9dbdbaf9
Pacôme Françon <pacome.francon@epitech.eu>
2018-11-22 18:04:25 +0100

    merge zetapush + vuejs

--- hello-world-vue/src/App.vue
+++ hello-world-vue/src/App.vue
@@ -1,7 +1,6 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <HelloWorld/>
   </div>
 </template>
 
@@ -17,12 +16,50 @@ export default {
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
+html {
+  box-sizing: border-box;
+  height: 100%;
+}
+*,
+*::before,
+*::after {
+  box-sizing: inherit;
+  margin: 0;
+  padding: 0;
+}
+body {
+  font-family: Arial, Helvetica, sans-serif;
+  margin: 0;
+  height: 100%;
+}
+body::before {
+  display: block;
+  content: "Open Your Console";
+  background: #000;
+  color: #FFF;
+  position: fixed;
+  transform: rotate(45deg) translate(60px, 0px);
+  right: 0;
+  top: 0;
+  height: 2rem;
+  padding: 5px 30px;
+  line-height: 25px;
+  font-family: Calibri, Helvetica, Arial;
+  font-weight: bold;
   text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+}
+body:not(.connected) * {
+  opacity: 0.85;
+  pointer-events: none;
+  cursor: not-allowed;
+  color: graytext;
+}
+body.connected .ui-Logo {
+  animation: zoom 2s infinite;
+}
+@keyframes zoom {
+  0%   { transform: scale(1); }
+  50%  { transform: scale(1.1); }
+  100% { transform: scale(1); }
 }
 </style>
--- hello-world-vue/src/components/HelloWorld.vue
+++ hello-world-vue/src/components/HelloWorld.vue
@@ -1,56 +1,101 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+  <div id="main">
+    <header>
+      <img class="ui-Logo" src="@/assets/vueLogo.png">
+      <img class="ui-Logo" src="@/assets/zetapushLogo.png">
+      <h1>Welcome to ZetaPush with Vue.js</h1>
+    </header>
+    <main>
+      <h2>To get started, edit worker/index.ts and save to reload.</h2>
+      <nav>
+        <button :click="onClick">call hello()</button>
+      </nav>
+    </main>
+    <button :click="onClick">call hello()</button>
   </div>
 </template>
 
 <script>
-export default {
-  name: 'HelloWorld',
-  props: {
-    msg: String
+
+  // import { WeakClient } from '@zetapush/client';
+
+  // const client = new WeakClient({
+  //   platformUrl: 'https://celtia.zetapush.com/zbo/pub/business',
+  //   appName: 'a9i002ef',
+  // });
+  // const api = client.createProxyTaskService();
+
+  // client.connect().then(() => {
+  //   console.log('mdr');
+  //   document.getElementById('main').classList.add('connected')
+  // });
+
+  // var mdr = 'lol';
+
+  export default {
+    name: 'HelloWorld',
+    data: () => {
+      return {
+        mdr: 'lol'
+      };
+    },
+    methods: {
+      onClick: async function() {
+        console.log(await api.hello());
+      }
+    },
+    created: function () {
+      console.log(this.mdr);
+    }
   }
-}
+
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-h3 {
-  margin: 40px 0 0;
+<style>
+header {
+  background-color: #222;
+  height: 15rem;
+  padding: 2rem;
+  color: white;
+  text-align: center;
+  flex-direction: column;
 }
-ul {
-  list-style-type: none;
-  padding: 0;
+img {
+  margin: auto;
+  max-height: 75%;
 }
-li {
-  display: inline-block;
-  margin: 0 10px;
+h1, h2 {
+  font-weight: normal;
+  text-align: center;
 }
-a {
-  color: #42b983;
+h1 {
+  font-size: 1.5rem;
+  padding: 1.5rem 0;
+}
+h2 {
+  font-size: 1.25rem;
+  padding: 1.25rem 0;
+}
+main, nav {
+  display: flex;
+  flex-direction: column;
+}
+main {
+  padding: 1rem;
+}
+button, label {
+  margin: 0.5rem 1rem;
+  height: 3rem;
+  font-size: calc(3rem/2);
+  background: linear-gradient(rgb(216, 216, 216), rgb(186, 186, 186));
+  border-color: rgb(117, 117, 117) rgb(107, 107, 107) rgb(97, 97, 97);
+  border-style: solid;
+  border-width: 1px;
+  padding: 1px 7px 2px;
+  text-align: center;
+  cursor: pointer;
+  box-sizing: border-box;
+  align-items: flex-start;
 }
 </style>
